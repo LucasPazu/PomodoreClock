5bb35f8c39b50c877ea23061866f2e1137b60ec9
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2019-06-26 15:03:46 +0200

    timer's work

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,8 +1,9 @@
+
 // DOM Elements
 const time = document.querySelector('.time');
 const timeBar = document.querySelector('.time__bar');
 const mins = document.querySelector('.minutes');
-const timer = document.querySelector('.clock__timer');
+const clock = document.querySelector('.clock__timer');
 
 const startBtn = document.querySelector('#start');
 const resetBtn = document.querySelector('#reset');
@@ -10,7 +11,9 @@ const resetBtn = document.querySelector('#reset');
 // Setting the timer
 let isMouseDown = false;
 const timeWidth = 500;
-let minutes;
+let minutes = parseInt(mins.textContent);
+let isRunning = false;
+
 
 function mouseDown() {
     isMouseDown = true;
@@ -28,27 +31,69 @@ function chooseTime(e) {
         mins.textContent = minutes + ' min';
         const minutesPerc = Math.round(minutes / 60 * 100) + '%';
         timeBar.setAttribute('style', 'width: ' + minutesPerc);
-        console.log(minutes);
     }
+    else {
+        minutes = parseInt(mins.textContent)
+    }
+
 
 }
 
 // Countdown
-function startCountdown() {
+function startTimer() {
+    const seconds = minutes * 60;
+    timer(seconds);
 }
 
-function resetCountDown(){
-    timer.textContent = '00:00';
-    timeBar.setAttribute('style', 'width: 33%');
-    mins.textContent = '20 min';
+function timer(seconds) {
+    // clear any existing timers
+    if(isRunning === true)clearInterval(countdown);
+
+    isRunning = true;
+    const now = Date.now();
+    const then = now + seconds * 1000 // now is in milisconds
+    displayTimeLeft(seconds)
+    //displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            isRunning = false;
+            return;
+        }
+        console.log(secondsLeft);
+        // display
+        displayTimeLeft(secondsLeft);
+    }, 1000);
 }
 
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds =  seconds %= 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' + remainderSeconds : remainderSeconds}`;
+    clock.textContent = display;
+    document.title = `${display} Pomodoro Clock`;
+    //console.log(seconds)
+}
+
+function resetCountDown() {
+    if(isRunning === true) {
+        clearInterval(countdown);
+        minutes = parseInt(mins.textContent);
+        isRunning = false;
+        clock.textContent = '00:00';
+        document.title = 'Pomodoro Clock';
+    }
+
+}
 // Events Listeners
 
 time.addEventListener('mousemove', chooseTime);
 window.addEventListener('mousedown', mouseDown);
 window.addEventListener('mouseup', mouseUp);
 
-startBtn.addEventListener('click', startCountdown);
+startBtn.addEventListener('click', startTimer);
 resetBtn.addEventListener('click', resetCountDown)
 
